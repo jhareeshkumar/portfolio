fbb264f2b381e99807caac05333797db1de3af28
jhareeshkumar <jhareesh30@gmail.com>
2024-02-02 18:18:44 +0530

    added header footer contact components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'contact',component:ContactComponent
+    }
+];
