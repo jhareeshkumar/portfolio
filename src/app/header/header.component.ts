c271c4b01c43f0d16f0b6c5e1eecfe8d8ca13c0b
jhareeshkumar <jhareesh30@gmail.com>
2024-02-12 19:37:09 +0530

    nav bar and footer added for testing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuOpen = false; // Flag to track menu state
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen; // Toggle menu state
+  }
 
 }
